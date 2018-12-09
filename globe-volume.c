e10057ca767bf029fed50c07a69f3613d6715100
vele2000plus <44909015+vele2000plus@users.noreply.github.com>
2018-12-09 21:14:46 +0200

    Create globe-volume.c

---
+++ globe-volume.c
@@ -0,0 +1,17 @@
+
+{
+int aktina;
+<<<<<<< Globe-with-float
+  float pi, result;
+    aktina=4;
+    pi = 3.1415926;
+    result = ((4/3) * pi * (aktina * aktina * aktina));
+    printf("4/3 * pi * (aktina*aktina*aktina) = %f\n", result);
+=======
+  double pi, result;
+    aktina=4;
+    pi = 3.14159265358979323846;
+    result = ((4/3) * pi * (aktina * aktina * aktina));
+    printf("4/3 * pi * (aktina*aktina*aktina) = %2.24f\n", result);
+>>>>>>> master
+}
